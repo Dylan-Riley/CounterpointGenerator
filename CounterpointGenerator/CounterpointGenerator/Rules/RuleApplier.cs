c0e308d0de3edcf2862c032aac24df587a649ca5
Dylan Riley <dylan@rileyz.net>
2021-07-27 15:57:48 -0700

    added interface for ruleapplier to follow design pattern

--- CounterpointGenerator/CounterpointGenerator/Rules/RuleApplier.cs
+++ CounterpointGenerator/CounterpointGenerator/Rules/RuleApplier.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace CounterpointGenerator
 {
-    class RuleApplier
+    public class RuleApplier: Rules.IRuleApplier
     {
         private List<IRules> ruleSet = new List<IRules>();
 
