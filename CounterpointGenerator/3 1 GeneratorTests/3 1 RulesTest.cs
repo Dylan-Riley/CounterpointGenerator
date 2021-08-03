b35fa3885337edbf9e0865da80d582f3b2e65b51
Dylan Riley <dylan@rileyz.net>
2021-08-02 23:34:24 -0700

    update existing tests to use new datatypes

--- CounterpointGenerator/3 1 GeneratorTests/3 1 RulesTest.cs
+++ CounterpointGenerator/3 1 GeneratorTests/3 1 RulesTest.cs
@@ -2,53 +2,80 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using CounterpointGenerator;
+using System.Linq;
 
 namespace _3_1_GeneratorTests
 {
     [TestClass]
     public class RulesTest
     {
-        private readonly List<int> defaultPossibilities = new List<int>()
+        private readonly List<int> intervalIntList = new List<int>()
         {
             -13, -12, -11, -10, -9, -8, -7, -6, -5,
             -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7,
             8, 9, 10, 11, 12, 13
         };
+        private List<Note> defaultNoteList = new List<Note>();
+
+        // Runs once before each test
+        [TestInitialize]
+        public void NoteListInit()
+        {
+            foreach (int pitch in intervalIntList)
+            {
+                defaultNoteList.Add(new Note(pitch));
+            }
+        }
+
+        // Runs once after each test
+        [TestCleanup]
+        public void TearDown()
+        {
+            defaultNoteList = new List<Note>();
+        }
+
         [TestMethod]
         public void Test_ImperfectConsonance()
         {
-            List<int> testRange = new List<int>(defaultPossibilities);
-            int spoofCurrentNote = 0; // C5
-            RuleInput ri = new RuleInput();
-            ri.CurrentNote = spoofCurrentNote;
-            ri.Possibilities = testRange;
+            List<Note> testRange = new List<Note>(defaultNoteList);
+            Note spoofCurrentNote = new Note(0); // C5
+            RuleInput ri = new RuleInput
+            {
+                CurrentNote = spoofCurrentNote,
+                Possibilities = testRange
+            };
 
             ImperfectConsonanceRule icr = new ImperfectConsonanceRule();
-            List<int> resultRange = icr.Apply(ri);
+            List<Note> resultRangeNote = icr.Apply(ri);
 
             List<int> expectedRange = new List<int>() { -9, -8, -4, -3, 3, 4, 8, 9 };
+            List<int> resultRange = new List<int>(resultRangeNote.Select(n => n.Pitch));
 
-            CollectionAssert.AreEquivalent(expectedRange, resultRange);
             Console.WriteLine(string.Join(", ", expectedRange));
             Console.WriteLine(string.Join(", ", resultRange));
+            CollectionAssert.AreEquivalent(expectedRange, resultRange);
         }
 
         [TestMethod]
         public void Test_PerfectConsonance()
         {
-            List<int> testRange = new List<int>(defaultPossibilities);
-            int spoofCurrentNote = 0; //C5
-            RuleInput ri = new RuleInput();
-            ri.CurrentNote = spoofCurrentNote;
-            ri.Possibilities = testRange;
+            List<Note> testRange = new List<Note>(defaultNoteList);
+            Note spoofCurrentNote = new Note(0); //C5
+            RuleInput ri = new RuleInput
+            {
+                CurrentNote = spoofCurrentNote,
+                Possibilities = testRange
+            };
 
             PerfectConsonanceRule pcr = new PerfectConsonanceRule();
-            List<int> resultRange = pcr.Apply(ri);
+            List<Note> resultRangeNote = pcr.Apply(ri);
 
             List<int> expectedRange = new List<int>(){ -12, -7, -5, 5, 7, 12 };
-            CollectionAssert.AreEquivalent(expectedRange, resultRange);
+            List<int> resultRange = new List<int>(resultRangeNote.Select(n => n.Pitch));
+
             Console.WriteLine(string.Join(", ", expectedRange));
             Console.WriteLine(string.Join(", ", resultRange));
+            CollectionAssert.AreEquivalent(expectedRange, resultRange);
         }
     }
 }
