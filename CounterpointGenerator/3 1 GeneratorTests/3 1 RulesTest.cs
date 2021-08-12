ad81c662843eb41af603ea359100d0381cb7b0b4
Dylan Riley <dylan@rileyz.net>
2021-08-12 15:50:56 -0700

    undo last commit, will conflict with more rules unit testing

--- CounterpointGenerator/3 1 GeneratorTests/3 1 RulesTest.cs
+++ CounterpointGenerator/3 1 GeneratorTests/3 1 RulesTest.cs
@@ -11,9 +11,9 @@ namespace _3_1_GeneratorTests
     {
         private readonly List<int> intervalIntList = new List<int>()
         {
-            -12, -11, -10, -9, -8, -7, -6, -5,
+            -13, -12, -11, -10, -9, -8, -7, -6, -5,
             -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7,
-            8, 9, 10, 11, 12
+            8, 9, 10, 11, 12, 13
         };
         private List<Note> defaultNoteList = new List<Note>();
 
@@ -38,7 +38,7 @@ namespace _3_1_GeneratorTests
         public void Test_ImperfectConsonance()
         {
             List<Note> testRange = new List<Note>(defaultNoteList);
-            Note spoofCurrentNote = new Note(Constants.C5);
+            Note spoofCurrentNote = new Note(0); // C5
             RuleInput ri = new RuleInput
             {
                 CurrentNote = spoofCurrentNote,
@@ -48,7 +48,7 @@ namespace _3_1_GeneratorTests
             ImperfectConsonanceRule icr = new ImperfectConsonanceRule();
             List<Note> resultRangeNote = icr.Apply(ri);
 
-            List<int> expectedRange = new List<int>(Constants.IMPERFECT_INTERVALS);
+            List<int> expectedRange = new List<int>() { -9, -8, -4, -3, 3, 4, 8, 9 };
             List<int> resultRange = new List<int>(resultRangeNote.Select(n => n.Pitch));
 
             Console.WriteLine(string.Join(", ", expectedRange));
